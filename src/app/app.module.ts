fdae3d493372f353846331dab0eb026b3d534fda
Khalid <ksnoman@live.com>
2021-10-26 08:32:44 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainResumeComponent } from './main-resume/main-resume.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainResumeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
