649192eb024acd0c94b733989bc430da60295224
Khalid <ksnoman@live.com>
2021-10-26 11:25:55 +0300

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainResumeComponent } from './main-resume/main-resume.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainResumeComponent,
-    ProjectsComponent,
-    NavBarComponent
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
